82e573cb23da9121a4212ad32bf257ecc0f731ec
stho32 <stho32github@gmail.com>
2021-12-19 18:39:10 +0100

    Experiment abgeschlossen

--- Source/index.js
+++ Source/index.js
@@ -1,1 +1,24 @@
-console.log("Bingo");
+var APIController = require("api-tools-ts").APIController;
+var api = new APIController("/random");
+api.AddEndPoint("/number", "get", function (req, res) {
+    res.json({
+        text: "number between 1-10",
+        number: Math.round(Math.random() * 10)
+    });
+});
+api.AddEndPoint("/number/:min,:max", "get", function (req, res) {
+    var min = Number(req.params.min);
+    var max = Number(req.params.max);
+    res.json({
+        text: "number between",
+        number: Math.round(Math.random() * (max - min)) + min
+    });
+});
+api.AddEndPoint("/animal", "get", function (req, res) {
+    var animals = ["Elephant", "Wuff", "Cat", "Tree"];
+    var randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+    res.json({
+        animal: randomAnimal
+    });
+});
+api.StartServer();
--- Source/index.ts
+++ Source/index.ts
@@ -1,1 +1,31 @@
-console.log("Bingo");
+const { APIController } = require("api-tools-ts");
+
+const api = new APIController("/random");
+
+api.AddEndPoint("/number", "get", (req, res) => {
+    res.json({
+        text: "number between 1-10",
+        number : Math.round(Math.random() * 10)
+    });
+});
+
+api.AddEndPoint("/number/:min,:max", "get", (req, res) => {
+    let min = Number(req.params.min);
+    let max = Number(req.params.max);
+
+    res.json({
+        text: "number between",
+        number : Math.round(Math.random() * (max-min)) + min
+    });
+});
+
+api.AddEndPoint("/animal", "get", (req, res) => {
+    let animals=["Elephant", "Wuff", "Cat", "Tree"];
+    let randomAnimal=animals[Math.floor(Math.random()*animals.length)];
+
+    res.json({
+        animal : randomAnimal
+    });
+});
+
+api.StartServer();
